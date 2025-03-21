07f34471aae944a3fe2f10ba6227cdce419800b6
AbishekKhanal <abishekkhanal2056@gmail.com>
2025-03-21 16:01:53 +0545

    Add ServicesCard component to fetch and display services dynamically

--- src/pages/Services/Services.jsx
+++ src/pages/Services/Services.jsx
@@ -1,8 +1,7 @@
 import React from "react";
 import { Banner } from "../../components/Banner";
 
-import services from "../../utils/servces.json";
-import cliniic from "../../assets/images/clinic-setup.png";
+import { ServicesCard } from "../../components/Services/ServicesCard";
 
 export const Services = () => {
   return (
@@ -13,26 +12,7 @@ export const Services = () => {
       </div>
       <div className="container my-5">
         <h4 className="text-center">Additional Services</h4>
-        <div className="row g-4 my-3">
-          {services.map((item, index) => (
-            <div className="col-lg-4">
-              <div className="card p-4">
-                <center>
-                  <img
-                    src={cliniic}
-                    alt={item.title}
-                    style={{ width: 48 }}
-                    className="my-3"
-                  />
-                </center>
-                <div className="card-body text-center mt-4">
-                  <h5>{item.title}</h5>
-                  <p>{item.description}</p>
-                </div>
-              </div>
-            </div>
-          ))}
-        </div>
+        <ServicesCard />
       </div>
     </div>
   );
