30fc6f49ca71254e089532bb70ed7af0dd539e72
AbishekKhanal <abishekkhanal2056@gmail.com>
2025-03-31 15:58:07 +0545

    Update Apollo Client URI to use production endpoint

--- src/utils/ApolloClient.js
+++ src/utils/ApolloClient.js
@@ -1,7 +1,7 @@
-import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "http://localhost:10003/graphql",
+  uri: "https://cms.allnepalclinic.com/graphql",
   cache: new InMemoryCache(),
 });
 
